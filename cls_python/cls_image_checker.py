d4664c01a29a6d38852027642278ef441b8c03d7
bisecattledb <bisecattledb@gmail.com>
2015-06-11 11:51:53 +0900

    remove led thread and add distance information to the camera

--- cls_python/cls_image_checker.py
+++ cls_python/cls_image_checker.py
@@ -7,26 +7,11 @@ from .main import ClsPython
 
 logger = logging.getLogger(__name__)
 
-def adjust_led(controller, stop):
-    logger.info("[START] adjust_led thread")
-    while True:
-        if stop():
-            controller.set_led("reset", 0)
-            break
-        pl_distance = controller.get_distance("pl")
-        no_pldistance = controller.get_distance("nopl")
-
-        controller.set_led("pl", pl_distance)
-        controller.set_led("nopl", no_pldistance)
-    logger.info("[FINISH] adjust_led thread")
-
 def image_checker():
     with ClsPython() as cls:
         pl_timeout = time.time() + 2
 
         while time.time() < pl_timeout:
-            pl_distance = cls.get_distance("pl")
-            cls.set_led("pl", pl_distance)
             cls.snap_and_save("pl")
 
         cls.set_led("reset", 0)
